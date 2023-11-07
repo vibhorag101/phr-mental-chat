0b30351a010682decbb6f8c3e8158f52ac58b5e2
Vibhor <vibhor20349@iiitd.ac.in>
2023-11-07 13:46:21 +0000

    Added the plutchik sentiment prediction

--- plutchik.py
+++ plutchik.py
@@ -3,26 +3,23 @@ import json
 import pandas as pd
 
 url = 'http://192.168.3.74:5000/predict_sentiment'
+def analyse_sentiment(text):
+    data = {'text': text_to_analyze}
+    response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    if response.status_code == 200:
+        result = response.json()
+        return(parse_response(result))
+    else:
+        print(f"Request failed with status code: {response.status_code}")
 
-# Define the text you want to analyze
-text_to_analyze = 'I am very happy today! The atmosphere looks cheerful '
+def parse_response(result):
+    data_dict = result["Your Data"][0]
+    keys = list(data_dict.keys())[0].split(",")
+    value = list(data_dict.values())[0].split(",")
+    predicted_sentiment = [keys[i].split(" ")[0] for i in range(len(keys)) if value[i] == "1.0"]
+    return predicted_sentiment
 
-# Create a dictionary with the text
-data = {'text': text_to_analyze}
-
-# Send a POST request to the API with the data as JSON
-response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
-
-# Check if the request was successful
-if response.status_code == 200:
-    result = response.json()
+if __name__ == "__main__":
+    text_to_analyze = "I am very happy today! The atmosphere looks cheerful."
+    result = analyse_sentiment(text_to_analyze)
     print(result)
-
-    # Convert the dictionary to a DataFrame
-    df = pd.DataFrame([result])
-
-    # Save the DataFrame to a CSV file
-    df.to_csv('response.csv', index=False)
-else:
-    print(f"Request failed with status code: {response.status_code}")
-    print(response.text)  # Print the response content for debugging
