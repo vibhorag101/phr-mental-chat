834a21993ae97f869601c5d574949ffd99de9261
Vibhor Agarwal <vibhor20349@iiitd.ac.in>
2023-11-25 05:26:04 +0000

    changed the new model

--- suicideModel/model.py
+++ suicideModel/model.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 # Using https://huggingface.co/SamLowe/roberta-base-go_emotions
 # Can classify 28 emotions
 def predict_suicide(text):
-    classifier = pipeline(task="text-classification", model="vibhorag101/PHR_Suicide_Prediction_Roberta",device_map="auto")
+    classifier = pipeline(task="text-classification", model="vibhorag101/roberta-base-suicide-prediction-phr",device_map="auto")
     emotions = classifier(text)
     return(emotions[0]['label'])
 
