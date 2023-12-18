b1b1c1f380cde8374fafff263fd0cbd24d8c021d
Vibhor Agarwal <vibhor20349@iiitd.ac.in>
2023-12-18 15:01:44 +0000

    Added probability scores for emotions

--- fastAPI/plutchikAPI.py
+++ fastAPI/plutchikAPI.py
@@ -35,11 +35,40 @@ class PredictSentiment(Resource):
         columns = df.columns[df.iloc[0] == 1.0]
         columns = [column.split(' ')[0] for column in columns]
 
-        # Remove the temporary CSV file
         os.remove(temp_csv_path)
-        return {"columns": columns}
+        return {"emotions": columns}
 
+class PredictSentimentScores(Resource):
+    def post(self):
+        item = Item(**request.get_json())
+        
+        # Create a temporary CSV file and write the input text to it
+        with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix='.csv') as temp_csv:
+            temp_csv.write('id_str,text,label\n')
+            temp_csv.write(f'{item.id_str},{item.text},{item.label}\n')
+            temp_csv_path = temp_csv.name
+        # Run the command
+        cmd = f'python run_classifier.py --load model/transformer_semeval.clf --data {temp_csv_path} --text-key "text" --model "transformer" --write-results result_train_mc.csv --tokenizer-type SentencePieceTokenizer --vocab-size 32000 --tokenizer-path model/ama_32k_tokenizer.model --batch-size 32'
+        subprocess.run(cmd, shell=True)
+
+        df = pd.read_csv('result_train_mc.csv')
+
+        # Make a dictionary with prob values for each emotion
+        emotions = {}
+        for column in df.columns:
+            # if column contains word 'prob'
+            if 'prob' in column:
+                # get the value of the column
+                value = df[column].values[0]
+                # get the emotion name
+                emotion = column.split(' ')[0]
+                # add the emotion and value to the emotions dictionary
+                emotions[emotion] = value
+
+        os.remove(temp_csv_path)
+        return {"emotions": emotions}
+
+api.add_resource(PredictSentimentScores, '/predict_sentiment_scores')
 api.add_resource(PredictSentiment, '/predict_sentiment')
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- testScripts/plutchik_api_test.py
+++ testScripts/plutchik_api_test.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 
 url = 'http://192.168.3.74:5000/predict_sentiment'
+url2 = 'http://192.168.3.74:5000/predict_sentiment_scores'
 def analyse_sentiment(text):
     data = {'text': text}
     response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
@@ -12,7 +13,15 @@ def analyse_sentiment(text):
     else:
         print(f"Request failed with status code: {response.status_code}")
 
+def analyse_sentiment_scores(text):
+    data = {'text': text}
+    response = requests.post(url2, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    if response.status_code == 200:
+        emotions = response.json().get('emotions', [])
+        return(emotions)
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+
 if __name__ == "__main__":
     print(analyse_sentiment("I am feeling very sad today."))
-    print(analyse_sentiment("I lost so much money in a fire at my factory."))
-    print(analyse_sentiment("In the tender embrace of shared moments, their souls danced in unity, weaving a tapestry of emotions that transcended words, creating a profound connection that resonated in the silent language of the heart."))
+    print(analyse_sentiment_scores("Are you a fool?"))
