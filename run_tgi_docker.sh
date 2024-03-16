b8233d90d6df18a2fa2bfc158fffcee95c293ac4
Vibhor Agarwal <vibhor20349@iiitd.ac.in>
2024-03-16 18:19:05 +0000

    Added auto image pull to run-tgi

--- run_tgi_docker.sh
+++ run_tgi_docker.sh
@@ -3,11 +3,17 @@
 # make executable using chmod u+x run_tgi.sh
 # run using ./run_tgi.sh
 # pull using docker pull ghcr.io/huggingface/text-generation-inference:sha-8a5bcba
+# retrieve the image_id using docker images
 # add the image_id to image_name variable
+image_repo="ghcr.io/huggingface/text-generation-inference"
+image_tag="sha-8a5bcba"
+docker pull $image_repo:$image_tag
+
+# image_name=287785ff9b29
+image_name=$(docker images -q | head -n 1)
 token="enter-token-here"
 volume=$PWD/data
 container_name=phr_ai_api
-image_name=287785ff9b29
 model=vibhorag101/llama-2-7b-chat-hf-phr_mental_therapy_v2
 MAX_TOTAL_TOKENS=4096
 MAX_INPUT_LENGTH=2048
